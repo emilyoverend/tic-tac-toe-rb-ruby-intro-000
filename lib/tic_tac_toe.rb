0f581bc8e02c6b76684657aa16bee531514ecf94
Emily Overend <eo17233@gmail.com>
2021-01-25 02:37:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,10 +47,11 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
-  valid_move?(board, input) == true
-    move(board, index)
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(board, input)
+    player_token = current_player(board)
+    move(board, index, player_token)
     display_board(board)
   else
     turn(board)
