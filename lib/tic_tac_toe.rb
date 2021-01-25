9d0624d0d4e3aac66e900abe50949de333002964
Emily Overend <eo17233@gmail.com>
2021-01-25 02:17:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,6 +68,15 @@ def turn_count(board)
   return turns
 end
 
+def current_player(board)
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
     index_0 = win_combo[0]
